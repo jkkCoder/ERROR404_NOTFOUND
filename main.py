a18313e368aaec1d97247c243388ecf41dd6f81d
Sumit Kulhari <sk4213@srmist.edu.in>
2022-10-28 18:05:31 +0530

    added program structure

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
 import pygame
+
+WIDTH, HEIGHT = 1200, 600
+WIN = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Subway Surfers")
+
+
+def main():
+    
+
+
+
+
+if __name__ == "__main__":
+    main()
+
